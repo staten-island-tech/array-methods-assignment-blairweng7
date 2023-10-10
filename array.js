4ffd5bbb372520b6df96bfa2078f7c5e1aa2b643
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-10-10 09:30:07 -0400

    Create array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+const people = [
+  {
+    firstName: "Dakota",
+    lastName: "Stabile",
+    DOB: 2007,
+    siblings: []
+  },
+  {
+    firstName: "Blair",
+    lastName: "Weng",
+    DOB: 2007,
+    siblings: []
+  }
+];
+
+console.log(people.DOB);
