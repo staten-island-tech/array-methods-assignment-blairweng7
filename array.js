9fb36d29c5a334d209913eb7c52a1c55dfe37a3d
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-10-10 23:41:33 -0400

    Update array.js

--- array.js
+++ array.js
@@ -1,16 +1,40 @@
-const people = [
+const colors = [
   {
-    firstName: "Dakota",
-    lastName: "Stabile",
-    DOB: 2007,
-    siblings: []
+    name: "Pink",
+    numbers: [1, 2, 3, 4],
+    year: 2023,
+    shade: "dark",
   },
   {
-    firstName: "Blair",
-    lastName: "Weng",
-    DOB: 2007,
-    siblings: []
+    name: "Purple",
+    numbers: [5, 6, 7, 8],
+    year: 2022,
+    shade: "light",
+  },
+  {
+    name: "Blue",
+    numbers: [9, 10, 11, 12],
+    year: 2021,
+    shade: "dark",
+  },
+  {
+    name: "Orange",
+    numbers: [13, 14, 15, 16],
+    year: 2020,
+    shade: "light",
+  },
+  {
+    name: "Green",
+    numbers: [17, 18, 19, 20],
+    year: 2019, 
+    shade: "dark",
   }
 ];
 
-console.log(people.DOB);
+colors.forEach((colors)=> console.log(colors.name))
+colors.forEach((colors)=> console.log(colors.numbers));
+function filterItems(arr, query) {
+  return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+console.log(filterItems(colors, "dark")); // ['apple', 'grapes']
+
